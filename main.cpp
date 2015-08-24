29ed077273ae21fc775dbbb98d6010183b1f766a
Chia Ying Lee <lchiaying.lee@gmail.com>
2015-08-23 22:29:42 -0400

    Added BTPS symmetric matrix class with multiplication operator and element access

--- main.cpp
+++ main.cpp
@@ -1,28 +1,127 @@
 #include <iostream>
 #include <vector>
+#include <armadillo>
 #include "symm_matrix_mult.h"
 
 using namespace std;
+using namespace arma;
+
+#ifdef MKL
+#define dspmv_ DSPMV
+#define dgemv_ DGEMV
+#endif
+
+extern "C" {
+    void dspmv_(const char * UPLO, const int * N, const double * ALPHA, double * AP, double * X,
+                const int * INCX, const double * BETA, double * Y, const int * INCY);
+    void dgemv_(const char * TRANS, const int * M, const int * N, const double * ALPHA, double * A, const int * LDA,
+                double * X, const int * INCX, const double * BETA, double * Y, const int * INCY);
+}
 
 int main()
 {
-    const int n = 5;
-    vector<double> A={1,0,0,0,0,
-                        1,0,0,2,
-                          1,0,0,
-                            1,0,
-                              1};  // A.size() = n*(n+1)/2
-    vector<double> x={0,1,2,3,4};
+    /* Compare multiplication using Binary tree-packed storage and single-pass on packed storage algorithms */
+    size_t n, m;
+    cout << "Enter n: ";    cin >> n;
+    cout << "Enter m: ";    cin >> m;
+
+    unsigned depth;
+    cout << "Enter tree depth (or 0 to automatically figure depth): ";
+    cin >> depth;
+    if (depth==0 || pow(2,depth) > n) {
+        depth = figureTreePackedDepth(n);
+        cout << "Using depth " << depth << "\n";
+    }
+
+    vec AP(n*(n+1)/2, fill::randn);
+    vector<double> A = conv_to< vector<double> >::from(AP);
+
+    clock_t clck = clock();
+    SymmetricMatrix<double> A_BTPS(n, depth);
+    A_BTPS.setFromPacked(AP);
+    clck = clock() - clck;
+    printf("Setting A_BTPS took %f seconds.\n", ((float)clck)/CLOCKS_PER_SEC);
+
+    mat X(n, m, fill::randn);
+
+    clck = clock();
+    mat Y = A_BTPS * X;
+    clck = clock() - clck;
+    printf("BTPS multiplication took %f seconds.\n", ((float)clck)/CLOCKS_PER_SEC);
+
+
+
+    mat A_full(n,n,fill::none);
+    vector<double>::iterator itr = A.begin();
+    for (size_t i = 0; i < n; i++) {
+        double a_ij = *itr++;
+        A_full(i,i) = a_ij;
+        for (size_t j = i+1; j < n; j++) {
+            double a_ij = *itr++;
+            A_full(i,j) = a_ij;
+            A_full(j,i) = a_ij;
+        }
+    }
+
+    clck = clock();
+    mat Y_fullt = A_full.t() * X;
+    clck = clock() - clck;
+    printf("Transposed full symmetric matrix multiplication took %f seconds.\n", ((float)clck)/CLOCKS_PER_SEC);
+
+
+
+    /* BLAS */
+    int N = 1000, M = 100, INCX = 1, INCY = 1;
+    double ALPHA = 1, BETA = 0;
+    char UPLO = 'L';
+    double Y_dspmv[N*M];
+
+    cout << "Check -1 ";
+    X.reshape(n*m, 1);
+    cout << "Check 0 ";
+    vector<double> Xvec = conv_to< vector<double> >::from( conv_to<vec>::from(X) );
+    cout << "Check 1 ";
+    clck = clock();
+    cout << "Check 2 ";
+    for (int i = 0; i < M; i++)
+        dspmv_(&UPLO, &N, &ALPHA, &A[0], &Xvec[N*i], &INCX, &BETA, &Y_dspmv[N*i], &INCY);
+    clck = clock() - clck;
+    printf("dspmv took %f seconds.\n", ((float)clck)/CLOCKS_PER_SEC);
+
+
+
+    /*  Check correctness of multiplication algorithms
+    n = 8;
+    vector<double> A={1,0,0,0,0,0,0,0,
+                        1,0,0,2,0,0,0,
+                          1,0,0,0,0,0,
+                            1,0,0,0,0,
+                              1,0,0,0,
+                                1,0,0,
+                                  1,0,
+                                    1};  // A.size() = n*(n+1)/2
+
+    vector<double> x={0,1,2,3,4,5,6,7};
     vector<double> y(n);
 
     SymmMat_Vec_Mult(n, A, x, y);
-
-    // print result
     cout << "\nA * x = ( ";
-    for (int i = 0; i < n-1; i++) {
-        cout << y[i] << ", ";
-    }
-    cout << y[n-1] << ")^T";
+    for (size_t i = 0; i < n-1; i++)   cout << y[i] << ", ";
+    cout << y[n-1] << ")^T\n";
+
+    // Test setting A_BTPS from A
+    vec AP = conv_to<vec>::from(A);
+    A_BTPS.setFromPacked(AP);
+
+    A_BTPS.diagUpper_->fullPart_->print("diagUpper");
+    A_BTPS.fullPart_->print("fullPart");
+    A_BTPS.diagLower_->fullPart_->print("diagLower");
+
+    mat X = conv_to<mat>::from(x);
+    (A_BTPS * X).t().print("A_BTPS * X = ");
+
+    */
+
 
     return 0;
 }
--- symm_matrix_mult.h
+++ symm_matrix_mult.h
@@ -3,72 +3,166 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 #include <cmath>
 #include <armadillo>
 
 using namespace std;
+using namespace arma;
+
+
+// Choose a suitable depth. The smallest subblock will have dimension no larger than max_base_dim.
+unsigned figureTreePackedDepth(unsigned dim, unsigned max_base_dim = 8) {
+    return ((unsigned) ceil( log2(dim * 1. / max_base_dim) )) ;
+}
+
 
 
 /*
 *   Symmetric matrix class, implemented in binary-tree-packed storage (BTPS)
 */
-unsigned figureTreePackedDepth(unsigned dim, unsigned max_base_dim = 64, unsigned max_depth = 5) {
-    return min(max_depth, ((unsigned)ceil( log2(dim * 1. / max_base_dim) ) ) );
-}
-
 
 template <class T>
 class SymmetricMatrix {
-private:
-    arma::Mat<T> fullPart_;         // )    A = [ diagUpper     fullPart  ]
-    SymmetricMatrix * diagUpper_;   // )        [ fullPart.t    diagLower ]
-    SymmetricMatrix * diagLower_;   // )
-    size_t dim_;                    // dimension of matrix
-    unsigned depth_;                // recursive depth of tree-packed storage structure
+public:
+    Mat<T> * fullPart_;                 // )    A = [ diagUpper   fullPart.t ]
+    SymmetricMatrix<T> * diagUpper_;    // )        [ fullPart    diagLower  ]
+    SymmetricMatrix<T> * diagLower_;    // )
+    size_t dim_;                            // dimension of matrix
+    unsigned depth_;                        // recursive depth of tree-packed storage structure
+    size_t idx_begin_, idx_mid_, idx_end_;  // If part of a supermatrix, keep track of subindices within the supermatrix
 
 public:
     SymmetricMatrix() {};
-    SymmetricMatrix(size_t dim, unsigned depth);
-    ~SymmetricMatrix() { if (depth_) { delete [] diagLower_, diagUpper_;} };
+    SymmetricMatrix(size_t dim, unsigned depth, string fill_ty, size_t idx_begin);
+    ~SymmetricMatrix() { delete fullPart_; if (depth_) {delete diagLower_; delete diagUpper_; }  };
 
-    arma::Mat<T> operator* (const arma::Mat<T> & X);
+    Mat<T> operator* (const Mat<T> & X);
+
+    T getij(size_t i, size_t j);
+    void setij(size_t i, size_t j, T value);
+    void setFromPacked(const Col<T> & AP);
 };
 
+
 template <class T>
-SymmetricMatrix<T>::SymmetricMatrix(size_t dim, unsigned depth) : dim_(dim), depth_(depth) {
+SymmetricMatrix<T>::SymmetricMatrix(size_t dim, unsigned depth, string fill_ty = "none", size_t idx_begin = 0) :
+    dim_(dim), depth_(depth), idx_begin_(idx_begin), idx_end_(idx_begin + dim) {
     if (depth_) {
-        size_t mid = dim_ / 2 + 1;
-        fullPart_ = arma::randu< arma::Mat<T> >(mid, dim_ - mid);
-        diagUpper_ = new SymmetricMatrix(mid, depth_ - 1);
-        diagLower_ = new SymmetricMatrix(dim_ - mid, depth_ - 1);
-    } else {
-        fullPart_ = arma::randu< arma::Mat<T> >(dim_, dim_);
-    }
+        size_t mid = dim_/2;
+        idx_mid_ = idx_begin + mid;
+
+        fullPart_ = new Mat<T>(dim_ - mid, mid, fill::none);
+        if (fill_ty == "randn")         fullPart_->randn();
+        else if (fill_ty == "zeros")    fullPart_->zeros();
+
+        diagUpper_ = new SymmetricMatrix<T>(mid, depth_ - 1, fill_ty, idx_begin_);
+        diagLower_ = new SymmetricMatrix<T>(dim_ - mid, depth_ - 1, fill_ty, idx_begin_ + mid);
+    } else
+        fullPart_ = new Mat<T>(dim_, dim_, fill::none);
+        if (fill_ty == "zeros")         fullPart_->zeros();
+        else if (fill_ty == "randn"){   fullPart_->randn(); *fullPart_ += (*fullPart_).t(); }
 }
 
+
+/* Algebraic operator */
 template <class T>
-arma::Mat<T> SymmetricMatrix<T>::operator* (const arma::Mat<T> & X) {
+Mat<T> SymmetricMatrix<T>::operator* (const Mat<T> & X) {
     if (depth_)
     {
-        size_t mid = dim_ / 2 + 1;
+        size_t mid = dim_ / 2;
 
-        arma::Mat<T> Y(X.n_rows, X.n_cols, arma::fill::none);
-        arma::Mat<T> Xupper = X.submat( arma::span(0, mid-1), arma::span::all );
-        arma::Mat<T> Xlower = X.submat( arma::span(mid, dim_-1), arma::span::all );
+        Mat<T> Y(X.n_rows, X.n_cols, fill::none);
+        Mat<T> Xupper = X.submat( span(0, mid-1), span::all );
+        Mat<T> Xlower = X.submat( span(mid, dim_-1), span::all );
 
-        Y.submat( arma::span(0, mid-1), arma::span::all ) = diagUpper_ * Xupper;
-        Y.submat( arma::span(0, mid-1), arma::span::all ) += fullPart_ * Xlower;
+        Y.submat( span(0, mid-1), span::all ) = *diagUpper_ * Xupper;
+        Y.submat( span(0, mid-1), span::all ) += (*fullPart_).t() * Xlower;
 
-        Y.submat( arma::span(mid, dim_-1), arma::span::all ) = diagLower_ * Xlower;
-        Y.submat( arma::span(mid, dim_-1), arma::span::all ) += fullPart_.t() * Xupper;
+        Y.submat( span(mid, dim_-1), span::all ) = *diagLower_ * Xlower;
+        Y.submat( span(mid, dim_-1), span::all ) += *fullPart_ * Xupper;
 
         return Y;
     }
-    else
-    {
-        return fullPart_ * X;
+    else return *fullPart_ * X;
+}
+
+/* Element access */
+template <class T>
+T SymmetricMatrix<T>::getij(size_t i, size_t j) {
+    if (i < idx_begin_ || i >= idx_end_ || j < idx_begin_ || j >= idx_end_) {
+        printf("Index error for (%i,%i)!\n", i, j);
+        return 0;
+    }
+    if (i < j) swap(i,j);
+
+    if (depth_) {
+        if (i < idx_mid_)       return diagUpper_->getij(i, j);
+        else if (j < idx_mid_)  return (*fullPart_)(i-idx_mid_, j-idx_begin_);
+        else                    return diagLower_->getij(i, j);
+    }
+    else return (*fullPart_)(i-idx_begin_, j-idx_begin_);
+}
+
+template <class T>
+void SymmetricMatrix<T>::setij(size_t i, size_t j, T value) {
+    if (i < idx_begin_ || i >= idx_end_ || j < idx_begin_ || j >= idx_end_) {
+        printf("Index error for (%i,%i)!\n", i, j);
+        return;
+    }
+    if (i < j) swap(i,j);
+
+    if (depth_) {
+        if (i < idx_mid_)       diagUpper_->setij(i, j, value);
+        else if (j < idx_mid_)  (*fullPart_)(i-idx_mid_, j-idx_begin_) = value;
+        else                    diagLower_->setij(i, j, value);
+    } else {
+        (*fullPart_)(i-idx_begin_, j-idx_begin_) = value;
+        (*fullPart_)(j-idx_begin_, i-idx_begin_) = value;
     }
 }
+
+
+template <class T>
+void SymmetricMatrix<T>::setFromPacked(const Col<T> & AP) {
+    if (AP.size() != dim_ * (dim_+1) / 2) {cout << "*** Error! Wrong input size. ***\n"; return;}
+
+    if (depth_)
+    {
+        size_t mid = dim_ / 2;
+
+        // Full part and upper diagonal
+        Col<T> AP_upper(mid * (mid+1) / 2);
+        for (size_t j = 0; j < mid; j++) {
+            size_t col_last = (2*dim_ - j) * (j+1) / 2;
+            size_t col_mid = col_last - (dim_-mid);
+            size_t col_first = col_last - (dim_-j); //(2*dim_ - j + 1) * j / 2;
+            size_t upper_col_last = (2*mid - j) * (j+1) / 2;
+            size_t upper_col_first = upper_col_last - (mid-j); // (2*mid - j + 1) * j / 2;
+
+            fullPart_->col(j) = AP.subvec(span( col_mid, col_last-1 ));
+            AP_upper(span( upper_col_first, upper_col_last-1 )) = AP.subvec(span( col_first, col_mid-1 ));
+        }
+        diagUpper_->setFromPacked(AP_upper);
+
+        // Lower diagonal part
+        Col<T> AP_lower = AP.subvec( span( (2*dim_ - mid + 1) * mid/2 , AP.size() - 1 ) );
+        diagLower_->setFromPacked(AP_lower);
+    }
+    else
+    {
+        typename Col<T>::const_iterator itr = AP.begin();
+        for (size_t j = 0; j < dim_; j++) {
+            (*fullPart_)(j,j) = *itr++;
+            for (size_t i = j + 1; i < dim_; i++) {
+                T a_ij = *itr++;
+                (*fullPart_)(i,j) = a_ij;
+                (*fullPart_)(j,i) = a_ij;
+            }
+        }
+    }
+}
+
 
 
 
